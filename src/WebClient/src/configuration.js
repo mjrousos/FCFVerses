4ab52e74c4cf334cefa81a0626a8482c80389df8
Mike Rousos <Mike@MJRousos.com>
2020-01-15 22:01:19 -0500

    Remove unneeded template files and update ReadMe and AAD B2C config for this app

--- src/WebClient/src/configuration.js
+++ src/WebClient/src/configuration.js
@@ -1,9 +1,10 @@
 export default {
-  appInsightsKey: "Foo",
+  appInsightsKey: "",
   authSettings: {
-    clientId: "",
-    appIdUri: "",
-    authority: "",
+    clientId: "4334cd09-5089-4a5e-89ac-ff6ffcb3efef",
+    appIdUri: "https://fcfauth.onmicrosoft.com/fcfverses",
+    authority:
+      "https://fcfauth.b2clogin.com/fcfauth.onmicrosoft.com/B2C_1_signup_signin",
     redirectUri: "",
     postLogoutRedirectUri: "",
     popup: false

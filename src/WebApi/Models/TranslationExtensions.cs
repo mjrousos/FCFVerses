1e1304b17dc8654d403b55aead7da16b1081a2da
Mike Rousos <mike@mjrousos.com>
2020-04-03 23:58:59 -0400

    Move `TranslationExtensions` to its own file

---
+++ src/WebApi/Models/TranslationExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi.Models
+{
+    public static class TranslationExtensions
+    {
+        /// <summary>
+        /// Gets copyright notices for different Bible translations.
+        /// </summary>
+        /// <param name="translation">The translation to get a copyright notice for.</param>
+        /// <returns>The copyright notice, or null if none exists.</returns>
+        public static string? GetCopyrightNotice(this Translations translation) => translation switch
+        {
+            // TODO
+            _ => null
+        };
+    }
+}
